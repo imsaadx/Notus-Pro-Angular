80d1ea00000cc06fc3a34ad8f167f983c6928fc0
Saad <107410498+imsaadx@users.noreply.github.com>
2022-11-06 22:00:49 +0500

    Commit from TeleportHQ

---
+++ src/app/pages/blog-post/blog-post.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'
+import { Title, Meta } from '@angular/platform-browser'
+
+@Component({
+  selector: 'blog-post',
+  templateUrl: 'blog-post.component.html',
+  styleUrls: ['blog-post.component.css'],
+})
+export class BlogPost {
+  raw4jxm: string = ' '
+
+  constructor(private title: Title, private meta: Meta) {
+    this.title.setTitle('BlogPost - Notus Pro')
+    this.meta.addTags([
+      {
+        property: 'og:title',
+        content: 'BlogPost - Notus Pro',
+      },
+    ])
+  }
+}
